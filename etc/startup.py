73a984cbb3c4f0fbdbd5facbfdec28d173f8ec23
skywind3000 <skywind3000@163.com>
2023-03-06 22:57:16 +0800

    update startup.py

--- etc/startup.py
+++ etc/startup.py
@@ -3,12 +3,16 @@ import sys
 if sys.version_info[0] >= 3:
     import collections
     if 'Callable' not in collections.__dict__:
-        if 'abc' in collections.__dict__:
+        try:
+            import collections.abc
             if 'Callable' in collections.abc.__dict__:
                 collections.Callable = collections.abc.Callable
+        except ImportError:
+            pass
     del collections
 
 import rlcompleter
+
 try:
     import readline
     readline.parse_and_bind('tab: complete')
